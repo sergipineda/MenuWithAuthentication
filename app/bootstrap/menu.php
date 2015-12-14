cbb6e6aa0b61641450e8bf52287203055e404eda
Sergi <spineda1994@gmail.com>
2015-12-14 16:54:54 +0100

    Menu

--- app/bootstrap/menu.php
+++ app/bootstrap/menu.php
@@ -7,15 +7,28 @@
 
 
 //home
-MenuWithAuthentication::menu
+MenuWithAuthentication::menu()
+    ->title('Home')
+    ->icon('fa-dashboard')
+    ->url('/home')
+    ->rol('home')
+    ->permission('home')
+    ->user('sergi pineda');
 
 //Another link
 
-MenuWithAuthentication::menu
+MenuWithAuthentication::menu()
+    ->title('Another Link')
+    ->user(5);
 
 //Menu Multileve
 
-MenuWithAuthentication::menu
-  MenuWithAuthentication::menu
-  MenuWithAuthentication::menu
+MenuWithAuthentication::menu()->title('Multilevel')->icon('fa-credit-card');
+  MenuWithAuthentication::menu()->title('Link in level 2')->icon('fa-briefcase');
+  MenuWithAuthentication::menu()->title('Link in level 2')->icon('fa-user');
+
+MenuWithAuthentication::menu()
+    ->title('Últim menú')
+    ->icon('fa-dashboard')
+    ->url('http://www.google.es');
 
